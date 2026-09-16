1a3fa8bb41d20495ad693ebf3e4183bd9879acf7
QvestionMark <139956040+QvestionMark@users.noreply.github.com>
2026-09-16 13:30:30 +0200

    intro for stage 1

--- scenes/stages/stage_1.gd
+++ scenes/stages/stage_1.gd
@@ -2,7 +2,15 @@ extends Stage
 
 @onready var PROJECTILE : PackedScene = preload("res://scenes/stages/stage_1_attacks/tooth.tscn")
 
+var is_ready = false
+
+func  _ready() -> void:
+	super()
+	await $AnimatedSprite2D.animation_finished
+	$AnimatedSprite2D.play("default")
+	is_ready = true
+
 func _on_timer_timeout() -> void:
-	if randf() < 1.0/3.0:
+	if is_ready and randf() < 1.0/3.0:
 		var projectile = PROJECTILE.instantiate() as AnimatableBody2D
 		add_child(projectile)
